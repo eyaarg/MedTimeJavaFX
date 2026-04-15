22a11e41972c6064beb55aaa47463da842404b5c
youssefnajjar <youssefnajj23@gmail.com>
2026-04-15 10:13:33 +0100

    initialisation launcher produit

--- src/main/java/esprit/fx/MainFxProd.java
+++ src/main/java/esprit/fx/MainFxProd.java
@@ -1,4 +1,28 @@
 package esprit.fx;
 
-public class MainFxProd {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class MainFxProd extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(
+                getClass().getResource("/fxml/ListProd.fxml")));
+        Scene scene = new Scene(root);
+        stage.setTitle("MedTimeFX");
+        stage.setScene(scene);
+        stage.setMaximized(true);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }

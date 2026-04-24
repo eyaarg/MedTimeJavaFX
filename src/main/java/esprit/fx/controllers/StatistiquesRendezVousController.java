97fbfe4bfcd9c3efd24ad3cdc5a2eb8b168a58db
TbiniFarah <farah.Tbini@esprit.tn>
2026-04-24 16:16:28 +0100

    Intégration du service dans le contrôleur et affichage des graphiques de statistiques

--- src/main/java/esprit/fx/controllers/StatistiquesRendezVousController.java
+++ src/main/java/esprit/fx/controllers/StatistiquesRendezVousController.java
@@ -23,8 +23,45 @@ public class StatistiquesRendezVousController implements Initializable {
     @FXML
     private Label tauxConfirmationLabel;
 
+    private StatistiquesService statsService = new StatistiquesService();
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // Logique d'initialisation à venir dans l'étape 3
+        chargerStatistiques();
+    }
+
+    private void chargerStatistiques() {
+        // 1. Taux de confirmation
+        double taux = statsService.getTauxConfirmation();
+        tauxConfirmationLabel.setText(String.format("%.2f %%", taux));
+
+        // 2. RDV par Mois (BarChart)
+        Map<String, Integer> moisMap = statsService.getNombreRDVParMois();
+        XYChart.Series<String, Number> seriesMois = new XYChart.Series<>();
+        seriesMois.setName("Nombre de RDV");
+        for (Map.Entry<String, Integer> entry : moisMap.entrySet()) {
+            seriesMois.getData().add(new XYChart.Data<>(entry.getKey(), entry.getValue()));
+        }
+        rdvParMoisChart.getData().clear();
+        rdvParMoisChart.getData().add(seriesMois);
+
+        // 3. RDV par Statut (PieChart)
+        Map<String, Integer> statutMap = statsService.getNombreRDVParStatut();
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+        for (Map.Entry<String, Integer> entry : statutMap.entrySet()) {
+            pieChartData.add(new PieChart.Data(entry.getKey() + " (" + entry.getValue() + ")", entry.getValue()));
+        }
+        rdvParStatutChart.setData(pieChartData);
+
+        // 4. RDV par Médecin (BarChart)
+        Map<String, Integer> medecinMap = statsService.getNombreRDVParMedecin();
+        XYChart.Series<String, Number> seriesMedecin = new XYChart.Series<>();
+        seriesMedecin.setName("RDV par Médecin");
+        for (Map.Entry<String, Integer> entry : medecinMap.entrySet()) {
+            seriesMedecin.getData().add(new XYChart.Data<>(entry.getKey(), entry.getValue()));
+        }
+        rdvParMedecinChart.getData().clear();
+        rdvParMedecinChart.getData().add(seriesMedecin);
+>>>>>>> 72dcbdb (Intégration du service dans le contrôleur et affichage des graphiques de statistiques)
     }
 }
